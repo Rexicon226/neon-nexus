d98b56e10387eb4c0b7fdbf4d399aea044cdbf7e
David Rubin <daviru007@icloud.com>
2023-10-06 22:37:05 -0700

    improved

--- build.zig
+++ build.zig
@@ -10,42 +10,58 @@ pub fn build(b: *Build) !void {
         else => |opt| opt,
     };
 
-    const program_name: []const u8 = b.option([]const u8, "program", "Which program to build from the src/programs directory") orelse return error.@"No Program was selected. Please run with -Dprogram=";
+    var files = std.ArrayList([]const u8).init(b.allocator);
+    defer files.deinit();
 
-    const neon_nexus_coff = b.addExecutable(.{
-        .name = "output.exe", // Name overriden later.
-        .target = .{
-            .cpu_arch = .x86,
-            .cpu_model = .{ .explicit = Cpu.Model.generic(.x86) },
-            .os_tag = .other,
-        },
-        .optimize = optimize,
-        .root_source_file = .{ .path = b.fmt("src/programs/{s}.zig", .{program_name}) },
-        .single_threaded = true,
-    });
+    var dir = try std.fs.cwd().openIterableDir("src/programs", .{});
+    var it = dir.iterate();
+    while (try it.next()) |file| {
+        if (file.kind != .file) {
+            continue;
+        }
+        try files.append(b.dupe(file.name));
+    }
 
-    neon_nexus_coff.addModule("dos", b.addModule("dos", .{
-        .source_file = .{ .path = "src/dos.zig" },
-    }));
+    for (files.items) |file_name| {
+        const neon_nexus_coff = b.addExecutable(.{
+            .name = "output.exe", // Name overriden later.
+            .target = .{
+                .cpu_arch = .x86,
+                .cpu_model = .{ .explicit = Cpu.Model.generic(.x86) },
+                .os_tag = .other,
+            },
+            .optimize = optimize,
+            .root_source_file = .{ .path = b.fmt("src/programs/{s}", .{file_name}) },
+            .single_threaded = true,
+        });
 
-    neon_nexus_coff.setLinkerScriptPath(.{ .path = "src/djcoff.ld" });
-    neon_nexus_coff.disable_stack_probing = true;
-    neon_nexus_coff.strip = true;
+        var file_split = std.mem.splitSequence(u8, file_name, ".");
 
-    const neon_nexus_exe_inputs = [_]Build.LazyPath{
-        .{ .path = "deps/cwsdpmi/bin/CWSDSTUB.EXE" },
-        neon_nexus_coff.addObjCopy(.{ .format = .bin }).getOutput(),
-    };
-    const neon_nexus_exe = FileRecipeStep.create(b, concatFiles, &neon_nexus_exe_inputs, .bin, b.fmt("{s}.exe", .{program_name}));
+        neon_nexus_coff.addModule("dos", b.addModule("dos", .{
+            .source_file = .{ .path = "src/dos.zig" },
+        }));
 
-    const installed_neon_nexus = b.addInstallBinFile(neon_nexus_exe.getOutput(), b.fmt("{s}.exe", .{program_name}));
-    b.getInstallStep().dependOn(&installed_neon_nexus.step);
+        neon_nexus_coff.setLinkerScriptPath(.{ .path = "src/djcoff.ld" });
+        neon_nexus_coff.disable_stack_probing = true;
+        neon_nexus_coff.strip = true;
 
-    const run_in_dosbox = b.addSystemCommand(&[_][]const u8{ "dosbox", "-conf", "dosbox_config.ini" });
-    run_in_dosbox.addFileArg(installed_neon_nexus.source);
+        const file_stripped = file_split.next().?;
 
-    const run = b.step("run", "Run the demo program in DOSBox");
-    run.dependOn(&run_in_dosbox.step);
+        const neon_nexus_exe_inputs = [_]Build.LazyPath{
+            .{ .path = "deps/cwsdpmi/bin/CWSDSTUB.EXE" },
+            neon_nexus_coff.addObjCopy(.{ .format = .bin }).getOutput(),
+        };
+        const neon_nexus_exe = FileRecipeStep.create(b, concatFiles, &neon_nexus_exe_inputs, .bin, b.fmt("{s}.exe", .{file_stripped}));
+
+        const installed_neon_nexus = b.addInstallBinFile(neon_nexus_exe.getOutput(), b.fmt("{s}.exe", .{file_stripped}));
+        b.step(file_stripped, b.fmt("Build the {s} program", .{file_stripped})).dependOn(&installed_neon_nexus.step);
+
+        const run_in_dosbox = b.addSystemCommand(&[_][]const u8{ "dosbox", "-conf", "dosbox_config.ini" });
+        run_in_dosbox.addFileArg(installed_neon_nexus.source);
+
+        const run = b.step(b.fmt("run-{s}", .{file_stripped}), b.fmt("Run the {s} program in DOSBox", .{file_stripped}));
+        run.dependOn(&run_in_dosbox.step);
+    }
 }
 
 fn concatFiles(_: *Build, inputs: []std.fs.File, output: std.fs.File) !void {
